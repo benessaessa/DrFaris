70ddae70a16c6592fd0e5e92e7097f23a5d9a21e
mohamed <mohamed.essa.elderwy@gmail.com>
2020-04-16 19:31:21 +0200

    home

---
+++ js/file.js
@@ -0,0 +1,54 @@
+function showMore()
+{
+    if ($('.threedots').css('display')=='none')
+    {
+        //already showing, we hide
+            $('.threedots').show();
+            $('.full_desc').hide();
+    }
+    else
+    {
+            //show more
+            $('.threedots').hide();
+            $('.full_desc').show();
+
+    }
+
+
+}
+function showMore2()
+{
+    if ($('.threedots1').css('display')=='none')
+    {
+        //already showing, we hide
+            $('.threedots1').show();
+            $('.full_desc1').hide();
+    }
+    else
+    {
+            //show more
+            $('.threedots1').hide();
+            $('.full_desc1').show();
+
+    }
+
+
+}
+function showMore3()
+{
+    if ($('.threedots3').css('display')=='none')
+    {
+        //already showing, we hide
+            $('.threedots3').show();
+            $('.full_desc3').hide();
+    }
+    else
+    {
+            //show more
+            $('.threedots3').hide();
+            $('.full_desc3').show();
+
+    }
+
+
+}
