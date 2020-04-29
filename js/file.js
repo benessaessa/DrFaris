bd82a7459864acbeb1bc984cf9232402d4f53451
mohamed <mohamed.essa.elderwy@gmail.com>
2020-04-29 14:47:55 +0200

    edits

--- js/file.js
+++ js/file.js
@@ -177,3 +177,16 @@ var galleryThumbs = new Swiper('.gallery-thumbs', {
       swiper: galleryThumbs
     }
   });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
